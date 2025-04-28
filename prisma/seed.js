6afb728893e38c996452f4902646cfa634228cc4
Jose Raphael Dela Cruz <delacruz2107393@ceu.edu.ph>
2025-04-28 17:57:19 +0800

    add task in seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -14,6 +14,7 @@ const userData = {
       { task: "Schedule Appointment" },
       { task: "Review for Exam" },
       { task: "Finish the Project" },
+      { task: "Modify Code" },
     ],
   },
 };
