7eb7fe944eb5f1fc679ad824c3ba2b1889f27884
Jose Raphael Dela Cruz <delacruz2107393@ceu.edu.ph>
2025-04-28 18:16:04 +0800

    Experimental

--- prisma/seed.js
+++ prisma/seed.js
@@ -15,6 +15,7 @@ const userData = {
       { task: "Review for Exam" },
       { task: "Finish the Project" },
       { task: "Modify Code" },
+      { task: "Experimental Code" },
     ],
   },
 };
